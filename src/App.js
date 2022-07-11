1f44bc3f6b9d5af9f063704f7f7ab911f8f14052
austinbhan <austinbhan@outlook.com>
2022-07-11 16:49:11 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -13,21 +13,28 @@ function App() {
     deck, passCard
   } = useGameContext();
   return (
-    <div className="App">
-      <section>
-        {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player player={1} hand={playerOneHand} /> 
-        {/* Player One Above is Reformatted */}
-        <Player to={to} player={2} hand={playerTwoHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
-        <Player to={to} player={3} hand={playerThreeHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
-        <CardList cards={deck} selectedCard={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} player={'deck'} />
-      </section>
-      <section>
-        {
-          selectedCard && <ExecutePassButton passCard={passCard} from={from} to={to} selectedCard={selectedCard} />
-        }
-      </section>
-    </div>
+    <>
+      <div>
+        <h1>
+          Card App
+        </h1>
+      </div>
+      <div className="App">
+        <section>
+          {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
+          <Player player={1} hand={playerOneHand} /> 
+          {/* Player One Above is Reformatted */}
+          <Player to={to} player={2} hand={playerTwoHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
+          <Player to={to} player={3} hand={playerThreeHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
+          <CardList cards={deck} selectedCard={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} player={'deck'} />
+        </section>
+        <section>
+          {
+            selectedCard && <ExecutePassButton passCard={passCard} from={from} to={to} selectedCard={selectedCard} />
+          }
+        </section>
+      </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Player 1/i);
+  const linkElement = screen.getByText(/Card App/i);
   expect(linkElement).toBeInTheDocument();
 });
