3f8ec2882f851a0c9f7c47c76dc6cb8715e781d1
austinbhan <austinbhan@outlook.com>
2022-07-11 14:44:15 -0700

    Reqired CardProvider.js

--- src/CardProvider.js
+++ src/CardProvider.js
@@ -24,8 +24,35 @@ export function CardProvider({ children }) {
     playerTwoHand, setPlayerTwoHand,
     playerThreeHand, setPlayerThreeHand,
     from, setFrom,
-    to, setTo
+    to, setTo,
+    passCard 
   };
+
+  function passCard(card) {
+    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+
+    // arrays start at zero, but our players start at 1 :shrug:
+    const toHand = playerHands[to - 1] || deck;
+    const fromHand = playerHands[from - 1] || deck;
+
+    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+
+    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+
+    toHand.push(cardToMove);
+
+    toSetFunction([...toHand]);
+    fromSetFunction([...fromHand]);
+
+    setSelectedCard(null);
+  }
+
+  function findCardIndex(value, suit, cards) {
+    return cards.findIndex(card => card.value === value && card.suit === suit);
+  }
 
   // Step 6
   return <GameContext.Provider value={stateAndSetters}>
