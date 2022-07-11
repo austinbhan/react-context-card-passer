d4eaeb53c3c4aaf6c0a1d243dc5353674e00c78a
austinbhan <austinbhan@outlook.com>
2022-07-11 12:35:18 -0700

    Step 6

--- src/CardProvider.js
+++ src/CardProvider.js
@@ -1,9 +1,13 @@
+// Step 1, Create File
+
 import { createContext, useContext, useState } from 'react';
 import initialCards from './cards-data';
 
 const GameContext = createContext();
 
+// Step 2
 export function GameProvider({ children }) {
+// Step 3
   const [deck, setDeck] = useState(initialCards);
   const [playerOneHand, setPlayerOneHand] = useState([]);
   const [selectedCard, setSelectedCard] = useState();
@@ -12,8 +16,24 @@ export function GameProvider({ children }) {
   const [from, setFrom] = useState('deck');
   const [to, setTo] = useState(1);
 
+  // Step 5
+  const stateAndSetters = {
+    deck, setDeck,
+    playerOneHand, setPlayerOneHand,
+    selectedCard, setSelectedCard,
+    playerTwoHand, setPlayerTwoHand,
+    playerThreeHand, setPlayerThreeHand,
+    from, setFrom,
+    to, setTo
+  };
+
+  // Step 6
+  return <GameContext.Provider value={stateAndSetters}>
+    {children}
+  </GameContext.Provider>;
 }
 
+// Step 4
 export function useGameContext() {
   return useContext(GameContext);
 }
