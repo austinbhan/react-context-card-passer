b18076c6d8405e85e35053f90b454298212fec27
austinbhan <austinbhan@outlook.com>
2022-07-11 16:41:50 -0700

    Player 1 for Test Paramter

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player player={1} hand={playerOneHand} />
+        <Player player={1} hand={playerOneHand} /> 
+        {/* Player One Above is Reformatted */}
         <Player to={to} player={2} hand={playerTwoHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
         <Player to={to} player={3} hand={playerThreeHand} setFrom={setFrom} selectedCard={selectedCard} setTo={setTo} setSelectedCard={setSelectedCard} />
         <CardList cards={deck} selectedCard={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} player={'deck'} />
